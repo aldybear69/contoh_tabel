b8963ca4d0af962a620e1746d965eb5e7adb4121
aldybear69 <aldybear69@gmail.com>
2023-06-29 23:13:27 +0700

    contoh tabel sederhana done

---
+++ android/app/src/main/kotlin/com/renjanaberuangstudio/contoh_tabel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.renjanaberuangstudio.contoh_tabel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contoh Tabel Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyTable(),
+    );
+  }
+}
+
+class MyTable extends StatefulWidget {
+  const MyTable({super.key});
+
+  @override
+  MyTableState createState() => MyTableState();
+}
+
+class MyTableState extends State<MyTable> {
+  final List<Map<String, dynamic>> tableData = [
+    {'Nama': 'John Doe', 'Usia': 25, 'Kota': 'New York', 'Selected': false},
+    {
+      'Nama': 'Jane Smith',
+      'Usia': 30,
+      'Kota': 'Los Angeles',
+      'Selected': false
+    },
+    {
+      'Nama': 'Michael Johnson',
+      'Usia': 35,
+      'Kota': 'Chicago',
+      'Selected': false
+    },
+    {
+      'Nama': 'Emily Davis',
+      'Usia': 28,
+      'Kota': 'San Francisco',
+      'Selected': false
+    },
+  ];
+
+  bool _sortAscending = true;
+  int _sortColumnIndex = 0;
+
+  void _sortTable(int columnIndex, bool ascending) {
+    setState(() {
+      _sortColumnIndex = columnIndex;
+      _sortAscending = ascending;
+
+      tableData.sort((a, b) {
+        if (ascending) {
+          return a.values
+              .toList()[columnIndex]
+              .compareTo(b.values.toList()[columnIndex]);
+        } else {
+          return b.values
+              .toList()[columnIndex]
+              .compareTo(a.values.toList()[columnIndex]);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contoh Tabel Flutter'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          sortAscending: _sortAscending,
+          sortColumnIndex: _sortColumnIndex,
+          columns: [
+            DataColumn(
+              label: const Text('Nama'),
+              tooltip: 'Nama Orang',
+              onSort: (columnIndex, ascending) {
+                _sortTable(columnIndex, ascending);
+              },
+            ),
+            DataColumn(
+              label: const Text('Usia'),
+              numeric: true,
+              onSort: (columnIndex, ascending) {
+                _sortTable(columnIndex, ascending);
+              },
+            ),
+            DataColumn(
+              label: const Text('Kota'),
+              onSort: (columnIndex, ascending) {
+                _sortTable(columnIndex, ascending);
+              },
+            ),
+          ],
+          rows: List.generate(
+            tableData.length,
+            (index) => DataRow(
+              selected: tableData[index]['Selected'],
+              onSelectChanged: (value) {
+                if (value!) {
+                  debugPrint('Baris "${tableData[index]['Nama']}" dipilih!');
+                  // Tambahkan tindakan saat baris diklik di sini
+                }
+                setState(() {
+                  tableData[index]['Selected'] = value;
+                });
+              },
+              cells: [
+                DataCell(Text(tableData[index]['Nama']),
+                    onTap: () => debugPrint('TEST AJA SIH')),
+                DataCell(Text(tableData[index]['Usia'].toString())),
+                DataCell(Text(tableData[index]['Kota'])),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
